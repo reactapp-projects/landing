3332ab930716f0395ec7201e65a694b717e18663
m.hoseein mirzaei <1380mirza@gmail.com>
2024-03-19 13:14:09 +0330

    Starting a react app project named landing

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Button>react app</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
