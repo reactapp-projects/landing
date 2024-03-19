baf485f322a6c1f76622656012ddcd3a75c3ee85
matin2001 <matin1380day@gmail.com>
2024-03-19 16:22:16 +0330

    fit: responsive for feature highlights fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FeatureHighlights from "./Components/FeatureHighlights";
+import ComingSoon from "./Components/ComingSoon";
 
 function App() {
   return (
     <div className="App">
       <div style={{ height: "20vh" }}>Header</div>
       <FeatureHighlights />
+      <div style={{ backgroundColor: "#736D6D", height: "0.1px" }}></div>
+      <ComingSoon />
     </div>
   );
 }
