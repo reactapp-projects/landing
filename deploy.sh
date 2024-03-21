f534c8974dda7f10a4773f0cf0e5381829b30159
m.hoseein mirzaei <1380mirza@gmail.com>
2024-03-21 23:57:36 +0330

    adding built zip

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 # git checkout main 
 # npm run build 
-.\pscp.exe -i "C:\Users\M.HOSEIN\Documents\GitHub\landing\Deep.pem" -r build/* ubuntu@ec2-13-50-245-228.eu-north-1.compute.amazonaws.com:/var/www/13.50.245.228/
+scp -i "./Deep.pem" -r build/* ubuntu@ec2-13-50-245-228.eu-north-1.compute.amazonaws.com:/var/www/13.50.245.228/
 
